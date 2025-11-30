367bbdfaa722611d0845fe86082ccf0182e35f16
Jithendra kornepati <jithendrakornepati1@gmail.com>
2025-11-30 12:00:00 +0530

    Added functionality in the viewmodel to update logout and delete account

--- app/src/main/java/uk/ac/tees/mad/reuse/presentation/HomeScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/reuse/presentation/HomeScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
